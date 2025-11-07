506ab709e8fe39d2f2dfe57a3f1f37a7b2632005
Sahil Agarwal <sahilagarwal20052005@gmail.com>
2025-11-07 23:30:55 +0530

    added remaining files

--- backend/auth/schemas.py
+++ backend/auth/schemas.py
@@ -1,10 +1,17 @@
 from pydantic import BaseModel, EmailStr, Field
-from typing import Optional
+from typing import Optional, List
+
+class FileStructure(BaseModel):
+    name: str
+    type: str  
+    id: str
+    children: Optional[List['FileStructure']] = None  
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=6)
     full_name: Optional[str] = None
+    fileStructure:Optional[FileStructure]={}
 
 class UserOut(BaseModel):
     id: str
