e355caa99f4c5e06335ca20a55fd395390c7d40a
Parth2412 <parthnpatil06@gmail.com>
2025-11-12 02:25:03 +0530

    fix

--- backend/lib/utils.py
+++ backend/lib/utils.py
@@ -1,18 +1,19 @@
 import inspect
 from typing import Dict, Literal, get_args, get_origin
+from pydantic import BaseModel
 from . import io_nodes
 from . import tables
 # from . import rag
 from . import alert
 
 
-def get_node_class_map() -> Dict[str, type]:
+def get_node_class_map():
     """
     Collect all node classes from lib/io_node.py, lib/tables.py, and lib/rag.py
     that define a class attribute `node_id`, and return a mapping
     of node_id -> class reference.
     """
-    node_class_map = {}
+    node_map = {}
     modules = [
         io_nodes,
         tables,
@@ -24,14 +25,16 @@ def get_node_class_map() -> Dict[str, type]:
         for name, cls in inspect.getmembers(module, inspect.isclass):
             if cls.__module__ != module.__name__:
                 continue
+            if not issubclass(cls,BaseModel):
+                continue
             if 'node_id' in cls.model_fields:
                 node_id_type = cls.model_fields['node_id'].annotation
                 if not (get_origin(node_id_type) is Literal):
                     continue
                 node_id_value = get_args(node_id_type)[0]
-                node_class_map[node_id_value] = cls
+                node_map[node_id_value] = cls
 
-    return node_class_map
+    return node_map
 
 
 node_map = get_node_class_map()
