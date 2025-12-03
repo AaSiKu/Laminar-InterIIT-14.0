794850228bc6c8afa44225471d147fc0e5cd29e0
Aaahana Mehrotra <aaahanamehrotra@Aaahanas-MacBook-Air.local>
2025-12-03 22:50:17 +0530

    slack dm

--- backend/Helper.py
+++ backend/Helper.py
@@ -7,4 +7,20 @@ env_path = Path('./api/') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-client.chat_postMessage(channel='#general', text="hi")
+
+
+def text_channel(channel_id, message):
+    response = client.chat_postMessage(
+        channel=channel_id,
+        text=message
+    )
+    return response
+
+
+def dm_user(user_id, message):
+    response = client.chat_postMessage(
+        channel=user_id,
+        text=message
+    )
+    return response
+
